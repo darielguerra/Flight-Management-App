d1196a28429a55f559f5ce11056afe2400c6aeb3
darielguerra <darielguerra1@gmail.com>
2024-03-25 19:37:33 -0400

    Navbar Update

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
 //import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import {Home, CreateAFlight, EditAFlight, UpdateAFlight, Error} from './pages';
-import { AppNav } from './features';
+import { Navbar } from './components/navbar/Navbar';
 
 function App() {
   return (
     <BrowserRouter>
-                <AppNav />
+                <Navbar />
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/createaflight" element={<CreateAFlight />} />
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,32 +1,37 @@
+import React from 'react';
 import './Home.css';
+
 
 export const Home = () => {
     return (
         <>  
         <html>
-        <div class="title">
+          <div class="title">
                 <h1>DGI Airlines</h1>
-        </div>
-        <div class="container">   
-  
-            <div class="createflightspage">
-                <a className="CreatFlights-link"
-                href="http://localhost:3000/createaflight"
-                target="_blank"
-                rel="noopener noreferrer">
-                <h3>Create A Flight</h3>
-                </a>       
-            </div>
+          </div>
 
-            <div class="findflightspage">
-                <a className="FindFlights-link"
-                href="http://localhost:3000/editaflight"
-                target="_blank"
-                rel="noopener noreferrer">
-                <h3>Edit A Flight</h3>
-                </a>
-            </div>
-        </div> 
+          <div class="container">   
+  
+                <div class="createflightspage">
+                    <a className="CreatFlights-link"
+                    href="http://localhost:3000/createaflight"
+                    target="_blank"
+                    rel="noopener noreferrer">
+                    <h3>Create A Flight</h3>
+                    </a>       
+                </div>
+
+                <div class="findflightspage">
+                    <a className="FindFlights-link"
+                    href="http://localhost:3000/editaflight"
+                    target="_blank"
+                    rel="noopener noreferrer">
+                    <h3>Edit A Flight</h3>
+                    </a>
+                </div>
+           </div> 
+          <div className="Sd-outer Sd-inner">    
+            </div>              
         </html>   
       </>
     );
