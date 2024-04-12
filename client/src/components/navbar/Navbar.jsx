451f08755c072a2fe8f67f1fba11c80370af10e9
darielguerra <darielguerra1@gmail.com>
2024-04-12 16:09:09 -0400

    Logo Update

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -6,10 +6,9 @@ export const Navbar = () => {
         <nav className="nav">
     
           <a className="logo" href="http://localhost:3000/" >
-           <img className="logo-image" src="images/Logo.png" alt="logo" />
-           
+           <img className="logo-image" src="images/Logo 4.png" alt="logo" />
           </a>
- <h1 className="logo-name">Zenith Airlines</h1>
+            <h1 className="logo-name">Zenith Airlines</h1>
           <div>
             <ul className="nav-links">
                 <li><a href="http://localhost:3000/createaflight">Create A Flight</a></li>
