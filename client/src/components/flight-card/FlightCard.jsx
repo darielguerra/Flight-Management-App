84a7126b8fb1e6b55f5db7814bc813dc0dca4dbb
darielguerra <darielguerra1@gmail.com>
2024-07-07 10:38:30 -0400

    Converted flight-card to its own component

---
+++ client/src/components/flight-card/FlightCard.jsx
@@ -0,0 +1,79 @@
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+import { EditIcon } from '../../assets/svg-icons/EditIcon';
+import { ArrowIcon } from '../../assets/svg-icons/ArrowIcon';
+import '../../pages/FlightList.css';
+
+export const FlightCard = (props) => {
+    
+  
+    console.log(props.flight.flightNumber);
+
+    const handleDelete = (key) => {
+      axios.delete(`http://localhost:8085/flights/${key}`)
+      .then (res => {
+          if ( res.status === "201") {
+              alert("deleted");
+              this.loadData();
+          } else {               
+          }
+      });
+  }
+
+    // Format date using the slice method, original format: yyyy-mm-dd
+    // slice syntax: slice(indexStart) or slice(indexStart, indexEnd) */    
+    const formatDate = (date) => {
+        //         month                  day               year: slicing from 0 up to but not including 4
+        date = date.slice(5, 7) + '/' + date.slice(8) + '/' + date.slice(0, 4);
+        return date;
+    }
+
+    // Format time     
+    const formatTime = (time) => {
+
+        // Splitting the time into hours and minutes
+        const [hours, minutes] = time.split(':');
+        
+        // Formatting the time with AM/PM
+        let formattedTime = '';
+        if (hours < 12) {
+            formattedTime = hours + ':' + minutes + ' AM';
+        } else {
+            formattedTime = (hours - 12) + ':' + minutes + ' PM';
+        }
+        
+        return formattedTime;
+    }
+
+    return (
+        <div key={props.flight._id} className="flight-card">            
+          <div className="flight-info">                         
+            <div className="flightnumber"><p>Flight# {props.flight.flightNumber}</p></div>
+            {/*<div className="flightnumber"><p><strong>Flight# </strong>{flight.flightNumber}</p></div>*/}
+            <div className="flight-details">
+              <div className="airports">{props.flight.departureAirport}<ArrowIcon /><br />
+                {props.flight.arrivalAirport}
+              </div>
+              <div className="pilot-departure-arrival">
+                <h2>Pilot</h2>
+                Ron Perlman
+              </div>
+              <div className="pilot-departure-arrival">
+                <h2>Departs</h2>
+                <p>{formatDate(props.flight.departureDate)}<br /> at {formatTime(props.flight.departureTime)}</p>
+                {/*<div><strong>Current Number Of Passengers:  </strong>{flight.currentNumberOfPassengers}</div>*/}                  
+              </div> 
+              <div className="pilot-departure-arrival">
+                <h2>Arrives</h2>
+                {formatDate(props.flight.arrivalDate)}<br /> at {formatTime(props.flight.arrivalTime)}
+                {/*<div><strong>Passenger Limit:  </strong>{flight.passengerLimit}</div>*/}
+              </div>   
+            </div>                   
+          </div>      
+          <div className="flight-buttons">
+            <button className="btn edit"><Link to={"/updateaflight/"+props.flight.flightNumber } className="update-text"><EditIcon /></Link></button>
+            <button onClick={() => handleDelete(props.flight.flightNumber)} className="btn delete">X</button>                    
+          </div>                       
+        </div>
+      )
+}
--- client/src/pages/FlightList.jsx
+++ client/src/pages/FlightList.jsx
@@ -1,58 +1,16 @@
 import axios from 'axios';
 import { useState, useEffect } from "react";
-import { Link } from 'react-router-dom';
-import { EditIcon } from '../components/icons/EditIcon';
-import { ArrowIcon } from '../components/icons/ArrowIcon';
+import { FlightCard } from '../components/flight-card/FlightCard';
 import './FlightList.css';
-
-import Card from '@mui/material/Card';
-import { useTheme } from '@mui/material/styles';
 
 export const FlightList = () => {  
 
     const [flights, setFlights] = useState([]);
-    const theme = useTheme();
-
+  
     useEffect(() => {
         axios.get('http://localhost:8085/flights')
             .then(res => setFlights(res.data));
     }, []);
-
-    const handleDelete = (key) => {
-        axios.delete(`http://localhost:8085/flights/${key}`)
-        .then (res => {
-            if ( res.status === "201") {
-                alert("deleted");
-                this.loadData();
-            } else {               
-            }
-        });
-    }
-
-    // Format date using the slice method, original format: yyyy-mm-dd
-    // slice syntax: slice(indexStart) or slice(indexStart, indexEnd) */    
-    const formatDate = (date) => {
-        //         month                  day               year: slicing from 0 up to but not including 4
-        date = date.slice(5, 7) + '/' + date.slice(8) + '/' + date.slice(0, 4);
-        return date;
-    }
-    
-    // Format time     
-    const formatTime = (time) => {
-
-        // Splitting the time into hours and minutes
-        const [hours, minutes] = time.split(':');
-        
-        // Formatting the time with AM/PM
-        let formattedTime = '';
-        if (hours < 12) {
-            formattedTime = hours + ':' + minutes + ' AM';
-        } else {
-            formattedTime = (hours - 12) + ':' + minutes + ' PM';
-        }
-        
-        return formattedTime;
-    }
 
     return (
       <div className="page" >        
@@ -64,35 +22,7 @@ export const FlightList = () => {
       <div className="flights">
         {flights.map((flight, index) => {
           return (
-            <div key={flight._id} className="flight-card">            
-              <div className="flight-info">                         
-                <div className="flightnumber"><p>Flight# {flight.flightNumber}</p></div>
-                {/*<div className="flightnumber"><p><strong>Flight# </strong>{flight.flightNumber}</p></div>*/}
-                <div className="flight-details">
-                  <div className="airports">{flight.departureAirport}<ArrowIcon /><br />
-                    {flight.arrivalAirport}
-                  </div>
-                  <div className="pilot-departure-arrival">
-                    <h2>Pilot</h2>
-                    Ron Perlman
-                  </div>
-                  <div className="pilot-departure-arrival">
-                    <h2>Departs</h2>
-                    <p>{formatDate(flight.departureDate)}<br /> at {formatTime(flight.departureTime)}</p>
-                    {/*<div><strong>Current Number Of Passengers:  </strong>{flight.currentNumberOfPassengers}</div>*/}                  
-                  </div> 
-                  <div className="pilot-departure-arrival">
-                    <h2>Arrives</h2>
-                    {formatDate(flight.arrivalDate)}<br /> at {formatTime(flight.arrivalTime)}
-                    {/*<div><strong>Passenger Limit:  </strong>{flight.passengerLimit}</div>*/}
-                  </div>   
-                </div>                   
-              </div>      
-              <div className="flight-buttons">
-                <button className="btn edit"><Link to={"/updateaflight/"+flight.flightNumber } className="update-text"><EditIcon /></Link></button>
-                <button onClick={() => handleDelete(flight.flightNumber)} className="btn delete">X</button>                    
-              </div>                       
-            </div>
+             <FlightCard flight={flight} key={flight._id}/>
           );
         })}
       </div>
