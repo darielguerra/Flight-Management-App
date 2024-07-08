8ea8e6b504f8ad237214a1c2d9a0669b10736eff
darielguerra <darielguerra1@gmail.com>
2024-07-08 19:11:42 -0400

    Updated navbar

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,16 +1,10 @@
 import React from 'react';
 import './Navbar.css';
-import { API } from "../../App"
-import { useTheme } from '@mui/material/styles';
 import { Link } from 'react-router-dom';
 
 
 export const Navbar = () => {
-
-   const theme = useTheme();
-
    return (
-
     <nav className="nav">          
       <a className="logo" href="/">
         <img className="logo-image" src="images/Logo 11.png" alt="logo" />
@@ -29,31 +23,3 @@ export const Navbar = () => {
     );   
 }
 
-
-
-      /*
-
-       <Box sex={{ flexGrow: 1}}>
-         <AppBar position="fixed" className="nav" style={{ backgroundColor: theme.palette.primary.main }}>
-          <Toolbar>
-          <a className="logo" href="http://localhost:3000/" >
-        <img className="logo-image" src="images/logo.png" alt="logo" />
-      </a>
-            
-            <Typography variant="h3" textAlign="center" component="div" sx={{ flexGrow: 1 }} className="title">
-               Zenith Airlines
-            </Typography> 
-            <IconButton
-              size="large"
-              edge="start"
-              color="inherit"
-              aria-label="menu"
-              sx={{ mr: 2 }}>
-              <MenuIcon />
-            </IconButton>
-          </Toolbar>
-         </AppBar>
-       </Box>
-
-
-*/
