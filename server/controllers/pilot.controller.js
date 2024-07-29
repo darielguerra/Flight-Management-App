ba49b84839f4a54baa420e3ec5a322c5ac07ef8c
darielguerra <darielguerra1@gmail.com>
2024-07-29 17:46:09 -0400

    Added the pilot controller

--- server/controllers/pilot.controller.js
+++ server/controllers/pilot.controller.js
@@ -1,2 +1,58 @@
 const Pilot = require('../models/Pilot.model');
 
+// get all pilots
+const getAllPilots = async () => {
+  const pilots = await Pilot.find();
+  return pilots;
+}
+
+// add a pilot
+const addPilot = async ({name, yearsOfService}) => {
+  try {
+      const pilot = new Pilot({
+          name,
+          yearsOfService
+      });
+      await pilot.save();
+
+      return pilot._id;
+  }
+  catch (err) {
+      console.error(err);
+      throw {status: 400, message: err};
+  }
+}
+
+//update airport
+const updatePilot = async ({name, yearsOfService}) => {
+  try {
+      const pilot = await Pilot.findOneAndUpdate({_id:_id},
+          {
+            name,
+            yearsOfService
+          }
+      );
+      if (pilot == null) {
+          throw `There is no pilot with id ${_id}`
+      }
+      return pilot;
+  }  
+  catch (err) {
+      console.error(err)
+      throw {status: 400, message: err}
+  }
+}
+
+//delete airport
+const deletePilot = async (id) => {
+  try {
+      const pilots = await Pilot.deleteOne({_id:id});
+      return pilots;
+  }
+  catch (err) {
+      console.error(err);
+      throw {status: 400, message: err};
+  }
+}
+
+module.exports = { getAllPilots, addPilot, updatePilot, deletePilot }
