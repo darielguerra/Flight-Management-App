254195aef6c5a5ee3c56781356f22e476548443e
darielguerra <darielguerra1@gmail.com>
2024-08-13 06:26:08 -0400

    Added About page

--- client/src/pages/about/About.jsx
+++ client/src/pages/about/About.jsx
@@ -6,12 +6,18 @@ export const About = () => {
       <div className="about-page">
         <div className="about-card">
           {/*<div className="about-card-inside"></div>*/}
-            <p>This application...</p>
-          
-        </div> 
-
-      </div>
-        
-      
+          <div className="about-title"><p>About this Site</p></div>
+            <div className="about-info">
+              <p><bold>Zenith Airlines</bold> is a fictional Airlines created to 
+                showcase a Flight Management App where the user can 
+                add, edit, and remove flights, pilots, and airports.  
+                The site was developed using React for the front end, 
+                Express.js and Node.js. for the back end, and MongoDB, 
+                for the database.  This site was created by 
+                <bold> Dariel Guerra</bold>.
+              </p>
+            </div>    
+        </div>
+      </div> 
     );
 }
