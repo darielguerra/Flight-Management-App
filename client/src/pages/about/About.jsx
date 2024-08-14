f086f10545cb11020efa0894b45575e2ec0e038f
darielguerra <darielguerra1@gmail.com>
2024-08-14 16:33:12 -0400

    Fixed About page typo

--- client/src/pages/about/About.jsx
+++ client/src/pages/about/About.jsx
@@ -8,7 +8,7 @@ export const About = () => {
           {/*<div className="about-card-inside"></div>*/}
           <div className="about-title"><p>About this Site</p></div>
             <div className="about-info">
-              <p><bold>Zenith Airlines</bold> is a fictional Airlines created to 
+              <p><bold>Zenith Airlines</bold> is a fictional airlines created to 
                 showcase a Flight Management App where the user can 
                 add, edit, and remove flights, pilots, and airports.  
                 The site was developed using React for the front end, 
