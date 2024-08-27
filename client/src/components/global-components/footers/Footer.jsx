c5786d02ff96979d095b103e4745e653f286d24d
darielguerra <darielguerra1@gmail.com>
2024-08-27 06:17:20 -0400

    Changed footer names

--- client/src/components/global-components/footers/Footer.jsx
+++ client/src/components/global-components/footers/Footer.jsx
@@ -4,7 +4,7 @@ export const Footer = () => {
   return (
     <div className="footer">
       <div className="footer-title">
-       © 2024 Alphon Labs
+       © 2024 Dariel Guerra
       </div>
       {/*Dariel Guerra 2024 © */}
     </div>
