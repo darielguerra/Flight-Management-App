e34179339478fbe5f0ab31d1ac8378c9fcb9ecd1
darielguerra <darielguerra1@gmail.com>
2024-08-28 06:17:29 -0400

    Added hover styles to FlightCard

--- client/src/components/flight-components/flight-card/FlightCard.jsx
+++ client/src/components/flight-components/flight-card/FlightCard.jsx
@@ -50,44 +50,54 @@ export const FlightCard = (props) => {
     }
 
     return (
-        <div key={props.flight._id} className="flight-card">    
-          <div className="flight-info">                         
-            <div className="flightnumber"><p>Flight# {props.flight.flightNumber}</p></div>
-            {/*<div className="flightnumber"><p><strong>Flight# </strong>{flight.flightNumber}</p></div>*/}
-            <div className="flight-details">
-              <div className="flight-card-airports">
-                <div className="departure-airport">
-                  <div className="departure-airport-name">{props.flight.departureAirport}</div>
-                  <div><ArrowIcon /></div>
+      <>
+
+        <div key={props.flight._id} className="flight-card">
+          
+          <div className="flight-details">
+            
+            <div className="flight-info">                         
+              <div className="flightnumber"><p>Flight# {props.flight.flightNumber}</p></div>
+              {/*<div className="flightnumber"><p><strong>Flight# </strong>{flight.flightNumber}</p></div>*/}
+              <div className="flight-details">
+                <div className="flight-card-airports">
+                  <div className="departure-airport">
+                    <div className="departure-airport-name">{props.flight.departureAirport}</div>
+                    <div><ArrowIcon /></div>
+                  </div>
+                  <div className="arrival-airport-name">{props.flight.arrivalAirport}</div>
                 </div>
-                <div className="arrival-airport-name">{props.flight.arrivalAirport}</div>
-              </div>
-              <div className="pilot-departure-arrival">
-                <h2>Pilot</h2>
-                Pete Mitchell
-              </div>
-              <div className="pilot-departure-arrival">
-                <h2>Departs</h2>
-                <p>{formatDate(props.flight.departureDate)}<br /> at {formatTime(props.flight.departureTime)}</p>
-                {/*<div><strong>Current Number Of Passengers:  </strong>{flight.currentNumberOfPassengers}</div>*/}                  
-              </div> 
-              <div className="pilot-departure-arrival">
-                <h2>Arrives</h2>
-                {formatDate(props.flight.arrivalDate)}<br /> at {formatTime(props.flight.arrivalTime)}
-                {/*<div><strong>Passenger Limit:  </strong>{flight.passengerLimit}</div>*/}
-              </div>   
-            </div>                   
-          </div>      
-          <div className="flight-buttons">
-            {/*<button className="btn edit"><Link to={`/updateaflight/${props.flight.flightNumber}`} state={props.flight.flightNumber}  className="update-text"><EditIcon /></Link></button>*/}
-            <button onClick={() => setUpdateModal(true)} className="btn edit"><EditIcon /></button>
-            {updateModal && <UpdateFlightModal flight={props.flight} closeModal={setUpdateModal} refresh={props.refresh}/>}
+                <div className="pilot-departure-arrival">
+                  <h2>Pilot</h2>
+                  Pete Mitchell
+                </div>
+                <div className="pilot-departure-arrival">
+                  <h2>Departs</h2>
+                  <p>{formatDate(props.flight.departureDate)}<br /> at {formatTime(props.flight.departureTime)}</p>
+                  {/*<div><strong>Current Number Of Passengers:  </strong>{flight.currentNumberOfPassengers}</div>*/}                  
+                </div> 
+                <div className="pilot-departure-arrival">
+                  <h2>Arrives</h2>
+                  {formatDate(props.flight.arrivalDate)}<br /> at {formatTime(props.flight.arrivalTime)}
+                  {/*<div><strong>Passenger Limit:  </strong>{flight.passengerLimit}</div>*/}
+                </div>   
+              </div>                   
+            </div>      
+            <div className="flight-buttons">
+              {/*<button className="btn edit"><Link to={`/updateaflight/${props.flight.flightNumber}`} state={props.flight.flightNumber}  className="update-text"><EditIcon /></Link></button>*/}
+              <button onClick={() => setUpdateModal(true)} className="btn edit"><EditIcon /></button>
+              {updateModal && <UpdateFlightModal flight={props.flight} closeModal={setUpdateModal} refresh={props.refresh}/>}
 
-            <button onClick={() => setDeleteModal(true)} className="btn delete">X</button>
-            {deleteModal && <DeleteModal flightNumber={props.flight.flightNumber} closeModal={setDeleteModal} delete={handleDelete} />}
+              <button onClick={() => setDeleteModal(true)} className="btn delete">X</button>
+              {deleteModal && <DeleteModal flightNumber={props.flight.flightNumber} closeModal={setDeleteModal} delete={handleDelete} />}
 
 
-          </div>                       
-        </div>
+            </div>                
+          </div>  
+          <div className="flight-border-line"></div>                
+        </div> 
+       
+        </>
+        
       )
 }
