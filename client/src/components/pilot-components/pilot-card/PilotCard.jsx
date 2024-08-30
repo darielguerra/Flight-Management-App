4ee9eada1c837da2e655103f83c9751d60abe29d
darielguerra <darielguerra1@gmail.com>
2024-08-30 17:15:54 -0400

    Combined EditDeletePilotModal and UpdatePilotModal into one: EditPilotModal

--- client/src/components/pilot-components/pilot-card/PilotCard.jsx
+++ client/src/components/pilot-components/pilot-card/PilotCard.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { EditDeletePilotModal } from '../pilot-modals/EditDeletePilotModal';
+import { EditPilotModal } from '../pilot-modals/EditPilotModal';
 import { PilotIcon } from '../../../assets/svg-icons/PilotIcon';
 import './PilotCard.css'
 
@@ -7,21 +7,21 @@ import './PilotCard.css'
 export const PilotCard = (props) => {
 
   const pilot = props.pilot;
-  const [editDeleteModal, setEditDeleteModal] = useState(false);
+  const [editPilotModal, setEditPilotModal] = useState(false);
   
   console.log(pilot);
 
   return (
     <div key={pilot._id} className="pilot-card">
       <div className="pilot-info">
-        <button onClick={() => setEditDeleteModal(true)} className="pilot-btn">
+        <button onClick={() => setEditPilotModal(true)} className="pilot-btn">
         {/*<img className="pilot-image" src="images/Pilot1.png" alt="logo"></img>*/}
         <PilotIcon />
         <div className="pilot-name"><p>{pilot.firstName}{" "}{pilot.lastName}</p></div>
         </button>
         <div className="line"></div>
       </div>
-      {editDeleteModal && <EditDeletePilotModal pilot={pilot} closeModal={setEditDeleteModal} refresh={props.refresh} />}
+      {editPilotModal && <EditPilotModal pilot={pilot} closeModal={setEditPilotModal} refresh={props.refresh} />}
     </div>
   )
 }
