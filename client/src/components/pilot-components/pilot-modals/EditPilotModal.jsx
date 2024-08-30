aa29649e89a459226176b10a89014459201c2048
darielguerra <darielguerra1@gmail.com>
2024-08-30 17:34:52 -0400

    Extracted UpdatePilot from EditPilotModal and made it a seperate component

--- client/src/components/pilot-components/pilot-modals/EditPilotModal.jsx
+++ client/src/components/pilot-components/pilot-modals/EditPilotModal.jsx
@@ -1,65 +1,22 @@
-import axios from 'axios';
-import { API } from "../../../App"
-import { useRef, useState } from "react";
-import { UpdatePilotModal } from './UpdatePilotModal';
+import { useState } from "react";
+import { UpdatePilot } from '../update-pilot/UpdatePilot';
 
 export const EditPilotModal = (props) => {
   
   const pilot = props.pilot;
-  const pilotId = props.pilot._id;
-
-  const firstName = useRef();
-  const lastName = useRef();
-  const yearsOfService = useRef();
-
   const [showUpdatePilot, setShowUpdatePilot ] = useState(false);
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    try {
-        await axios.put(`${API}/pilots/${pilotId}`, 
-        {
-          firstName:firstName.current.value,
-          lastName:lastName.current.value,
-          yearsOfService:yearsOfService.current.value
-        });
-        } 
-        catch (error) {
-          console.log(error);
-        }
-        props.closeModal(false);
-        props.refresh();
-        
-
-  }
 
   return (
     <div className="modal">
       <div onClick={() => props.closeModal(false)} className="overlay">
         <div onClick={(e) => e.stopPropagation()} className="edit-delete-pilot-modal">
+        {!showUpdatePilot && (
           <button onClick={() => setShowUpdatePilot(true)} className="edit-pilot-button">
             Edit
           </button>
-          {showUpdatePilot && 
-          
-          <form onSubmit={handleSubmit}>
-            <div className="pilot-first-name">
-              <label className="pilot-first-name-label">First Name:</label>
-              <input className="pilot-first-name-input" type="text" ref={firstName} />
-            </div>
-            <div className="pilot-last-name">
-              <label className="pilot-last-name-label">Last Name:</label>
-              <input className="pilot-last-name-input" type="text" ref={lastName} />
-            </div>
-            <div className="years-of-service">
-              <label className="years-of-service-label">Years of Service:</label>
-              <input className="years-of-service-input" type="text" ref={yearsOfService} />
-            </div>
-            <button type="submit">Update</button>
-          </form>
-          
-          
-          }
+        )}
+          {showUpdatePilot && <UpdatePilot pilotId={pilot._id} closeModal={props.closeModal} refresh={props.refresh} />}
         </div>          
       </div> 
     </div> 
---
+++ client/src/components/pilot-components/update-pilot/UpdatePilot.jsx
@@ -0,0 +1,47 @@
+import axios from 'axios';
+import { API } from "../../../App"
+import { useRef } from "react";
+
+export const UpdatePilot = (props) => {
+
+  const pilotId = props.pilotId;
+  console.log(pilotId);
+  const firstName = useRef();
+  const lastName = useRef();
+  const yearsOfService = useRef();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+        await axios.put(`${API}/pilots/${pilotId}`, 
+        {
+          firstName:firstName.current.value,
+          lastName:lastName.current.value,
+          yearsOfService:yearsOfService.current.value
+        });
+        } 
+        catch (error) {
+          console.log(error);
+        }
+        props.closeModal(false);
+        props.refresh();      
+  }  
+   
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="pilot-first-name">
+        <label className="pilot-first-name-label">First ame:</label>
+        <input className="pilot-first-name-input" type="text" ref={firstName} />
+      </div>
+      <div className="pilot-last-name">
+        <label className="pilot-last-name-label">Last Name:</label>
+        <input className="pilot-last-name-input" type="text" ref={lastName} />
+      </div>
+      <div className="years-of-service">
+        <label className="years-of-service-label">Years of Service:</label>
+        <input className="years-of-service-input" type="text" ref={yearsOfService} />
+      </div>
+      <button type="submit">Update</button>
+    </form>
+  )
+}
