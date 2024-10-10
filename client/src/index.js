a6e422019419a4f254f5cb34eec51c6e4034ad2b
darielguerra <darielguerra1@gmail.com>
2024-10-09 20:39:33 -0400

    Added Vercel Analytics

--- client/src/index.js
+++ client/src/index.js
@@ -4,9 +4,13 @@ import './global-styles/NormalizeCSS.css';
 import './global-styles/index.css';
 import './global-styles/modal-global-styles.css';
 import App from './App';
+import { Analytics } from '@vercel/analytics/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <>
+    <Analytics />
     <App />
+  </>
 );
 
