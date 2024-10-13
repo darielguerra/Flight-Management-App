6563e19abb4cdb64dc699b5605faa9ae0e4ed3f5
darielguerra <darielguerra1@gmail.com>
2024-10-13 09:21:26 -0400

    Modernized the styles for the Airport modal forms

--- client/src/components/airport-components/update-airport/UpdateAirport.jsx
+++ client/src/components/airport-components/update-airport/UpdateAirport.jsx
@@ -30,21 +30,20 @@ export const UpdateAirport = (props) => {
   return(
     <form onSubmit={handleSubmit} className="airport-modal-form">
       <div className="airport-code">
-        <label className="airport-code-label">          
-          Airport Code: <a className="code-link" href="https://www.leonardsguide.com/us-airport-codes.shtml" target="_blank" rel="noreferrer">(List)</a>
-        </label>        
-        <input className="airport-code-input" type="text" ref={code} />
+        {/*<label className="airport-code-label">Airport Code: </label>*/}           
+        <input className="airport-code-input" type="text" placeholder="Airport Code" ref={code} />  
+        <a className="code-link" href="https://www.leonardsguide.com/us-airport-codes.shtml" target="_blank" rel="noreferrer">List of Codes</a>       
       </div>
       <div className="airport-city">
-        <label className="airport-city-label">City:</label>
-        <input className="airport-city-input" type="text" ref={city} />
+        {/*<label className="airport-city-label">City:</label>*/}
+        <input className="airport-city-input airport-input" type="text" placeholder="City" ref={city} />
       </div>
       <div className="airport-state">
-        <label className="airport-state-label">State:</label>
-        <input className="airport-state-input" type="text" ref={state} />
+         {/*<label className="airport-state-label">State:</label>*/}
+        <input className="airport-state-input airport-input" type="text" placeholder="State" ref={state} />
       </div>
-      <div className="add-aiport-submit">
-        {/* <button onClick={handleSubmit} className="add-airport-submit-btn">Add</button> */}
+      <div className="add-airport-submit">
+         {/* <button onClick={handleSubmit} className="add-airport-submit-btn">Add</button> */}
         <button type="submit" className="add-airport-submit-btn">Add</button> 
       </div>
     </form>
