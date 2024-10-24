3d96af98cec5039f03ef7ca08c5971ae86feeaf3
darielguerra <darielguerra1@gmail.com>
2024-10-24 05:46:30 -0400

    Fixed DropdownButton icons not opening dropdown when clicked on

--- client/src/components/global-components/dropdown-menus/dropdown-button/DropdownButton.jsx
+++ client/src/components/global-components/dropdown-menus/dropdown-button/DropdownButton.jsx
@@ -3,22 +3,30 @@ import { forwardRef } from "react";
 import { FaChevronDown, FaChevronUp } from "react-icons/fa";
 import "../Dropdowns.css";
 
+import { ArrowIcon } from "../../../../assets/svg-icons/ArrowIcon";
+
 export const DropdownButton = React.forwardRef((props, ref ) => {
  
   /*fowardRef is a way of passing a ref to a component*/
 
   const { children, open, toggle } = props;
- 
+   
   return( 
-    <div 
-      onClick={toggle} 
-      className={`dropdown-btn ${open ?
-      "dropdown-btn-open" : null}`}
-      ref={ref}
-    >
-      {children}
-      <span className="dropdown-toggle-icon">
-        {open ? <FaChevronUp /> : <FaChevronDown />}        
-      </span>
-    </div>
+    <>
+      <div 
+        onClick={toggle} 
+        className={`dropdown-btn ${open ?
+        "dropdown-btn-open" : null}`}
+        ref={ref}
+      >
+        {children}
+        <span className="dropdown-toggle-icon">
+          {open ? <FaChevronUp />
+                  : <FaChevronDown />}       
+        </span>
+      </div>
+      <div onClick={toggle} className="toggle-icon-click-open"></div>
+      {/*onClick toggle not working on icons so place this div over it */}
+    </>
 )})
+
