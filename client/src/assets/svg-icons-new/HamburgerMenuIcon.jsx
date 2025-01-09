06a0f14ec80e943be5a0be1e74468ba76ec0bc9d
darielguerra <darielguerra1@gmail.com>
2025-01-09 17:34:09 -0500

    Added nav menu popup when HamburgerMenuIcon is selected

--- client/src/assets/svg-icons-new/HamburgerMenuIcon.jsx
+++ client/src/assets/svg-icons-new/HamburgerMenuIcon.jsx
@@ -1,11 +1,11 @@
 import "../../global-styles/icon-styles.css";
 
-export const HamburgerMenu = () => {
+export const HamburgerMenuIcon = () => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 24 24"
-      className="hamburger-menu"
+      className="hamburger-menu-icon"
     >
       <path
         d="M20 7L4 7"
--- client/src/components/global-components/navbar/Navbar.jsx
+++ client/src/components/global-components/navbar/Navbar.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { HamburgerMenu } from '../../../assets/svg-icons-new/HamburgerMenu';
+import { HamburgerMenuIcon } from '../../../assets/svg-icons-new/HamburgerMenuIcon';
 import './Navbar.css';
 
 export const Navbar = () => {
+ 
+  const [openHamburgerMenu, setOpenHamburgerMenu] = useState(false);
+
    return (
     <nav className="nav">          
       <a className="title" href="/"> 
@@ -16,8 +20,9 @@ export const Navbar = () => {
       <div>        
         <ul className="nav-links">
             {/*<li><Link to="/createaflight">Create A Flight</Link></li>*/}
-            <div className="nav-hamburger-menu">
-              <HamburgerMenu />
+            <div className="hamburger-menu-icon-container"
+                 onClick={() => setOpenHamburgerMenu(!openHamburgerMenu)}>
+              <HamburgerMenuIcon />
             </div>
             <li><Link to="/flights">Flights</Link></li>
             <li><Link to="/airports">Airports</Link></li>
@@ -25,6 +30,20 @@ export const Navbar = () => {
             <li><Link to="/about">About</Link></li>
         </ul>
       </div>
+      
+      {openHamburgerMenu && (
+        <div onClick={() =>setOpenHamburgerMenu(!openHamburgerMenu)} className="overlay">
+        <div onClick={(e) => e.stopPropagation()} className="hamburger-menu-window">
+          <ul className="hamburger-menu-nav-links">
+            <li><Link to="/flights">Flights</Link></li>
+            <li><Link to="/airports">Airports</Link></li>
+            <li><Link to="/pilots">Pilots</Link></li>
+            <li><Link to="/about">About</Link></li>
+          </ul>
+        </div>
+        </div>
+      )}
+
     </nav>
     );   
 }
