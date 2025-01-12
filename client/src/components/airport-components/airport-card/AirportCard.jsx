f76c810ab19ee8c614565deabf6083ef46eaecc1
darielguerra <darielguerra1@gmail.com>
2025-01-12 09:46:18 -0500

    Updated Navbar logo

--- client/src/components/airport-components/airport-card/AirportCard.jsx
+++ client/src/components/airport-components/airport-card/AirportCard.jsx
@@ -4,19 +4,19 @@ import './AirportCard.css'
 
 export const AirportCard = (props) => {
   
-  const [editPilotModal, setEditPilotModal] = useState(false);
+  const [editAirportModal, setEditAirportModal] = useState(false);
   const airport = props.airport;
   console.log(props.airport);
   
   return (
     <div key={airport._id} className="airport-card">
-      <button onClick={()=> setEditPilotModal(true)} className="airport-btn">
+      <button onClick={()=> setEditAirportModal(true)} className="airport-btn">
         <div className="airport-info">
           <div className="code">{airport.code}</div>
           <div className="city-state">{airport.city}, {airport.state}</div>
         </div>
       </button>
-      {editPilotModal && <EditAirportModal airport={airport} modalOpen={setEditPilotModal} refresh={props.refresh} />}
+      {editAirportModal && <EditAirportModal airport={airport} modalOpen={setEditAirportModal} refresh={props.refresh} />}
     </div>
   )
 
--- client/src/components/global-components/navbar/Navbar.jsx
+++ client/src/components/global-components/navbar/Navbar.jsx
@@ -9,21 +9,31 @@ export const Navbar = () => {
   const [openHamburgerMenu, setOpenHamburgerMenu] = useState(false);
 
    return (
-    <nav className="nav">          
+    <nav className="nav">     
+    {/*   
       <a className="title" href="/"> 
           <div className="heading-underline">
             <h1 className="heading">Zenith Airlines</h1>
           </div>
           <h3 className="subheading">Flight Management App</h3>
       </a>
+    */} 
+
+      <a className="nav-title" href="/"> 
+        <img className="nav-logo-img" src="images/NavBarWingMatchingBlue300pxWide.png" alt="Zenith Airlines" />
+        <div className="nav-title-initials">ZA</div>
+        <div className="nav-title-name">Fligth Management App</div>
+      </a>
+
+      <div className="hamburger-menu-icon-container"
+           onClick={() => setOpenHamburgerMenu(!openHamburgerMenu)}>
+        <HamburgerMenuIcon />
+      </div>
 
       <div>        
         <ul className="nav-links">
             {/*<li><Link to="/createaflight">Create A Flight</Link></li>*/}
-            <div className="hamburger-menu-icon-container"
-                 onClick={() => setOpenHamburgerMenu(!openHamburgerMenu)}>
-              <HamburgerMenuIcon />
-            </div>
+
             <li><Link to="/flights">Flights</Link></li>
             <li><Link to="/airports">Airports</Link></li>
             <li><Link to="/pilots">Pilots</Link></li>
